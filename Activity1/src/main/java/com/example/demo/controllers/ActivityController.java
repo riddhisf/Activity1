eb201725eb624df6869db9e012b442df68762002
chirag-2004 <chiraggupta.ddn@gmail.com>
2026-04-24 13:09:00 +0530

    Initial Commit

---
+++ Activity1/src/main/java/com/example/demo/controllers/ActivityController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/activity")
+public class ActivityController {
+	
+	@GetMapping("/chirag")
+	public String getActivity() {
+		return "Hello from Chirag!!";
+	}
+}
